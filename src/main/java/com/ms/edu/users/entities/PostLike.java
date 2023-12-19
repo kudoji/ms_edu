248cc4114ec95f87ccffebffadbe092cf0f47a4f
Your Name <you@example.com>
2023-12-19 21:09:39 +0300

    1.6 post likes entity & repo

---
+++ src/main/java/com/ms/edu/users/entities/PostLike.java
@@ -0,0 +1,35 @@
+package com.ms.edu.users.entities;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "users_subscriptions")
+public class PostLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long postId;
+    private Long likedByUserId;
+    private Date date;
+
+    public PostLike() {
+    }
+
+    public PostLike(Long postId, Long likedByUserId, Date date) {
+        this.postId = postId;
+        this.likedByUserId = likedByUserId;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "PostLike{" +
+                "id=" + id +
+                ", postId=" + postId +
+                ", likedByUserId=" + likedByUserId +
+                ", date=" + date +
+                '}';
+    }
+}
