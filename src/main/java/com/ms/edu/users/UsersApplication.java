3b4335ace63737d84043f60b9945e5233b8e3ce3
Your Name <you@example.com>
2023-12-19 20:38:04 +0300

    1.6 repositoryCheck()

--- src/main/java/com/ms/edu/users/UsersApplication.java
+++ src/main/java/com/ms/edu/users/UsersApplication.java
@@ -1,7 +1,11 @@
 package com.ms.edu.users;
 
+import com.ms.edu.users.entities.User;
+import com.ms.edu.users.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UsersApplication {
@@ -10,4 +14,18 @@ public class UsersApplication {
 		SpringApplication.run(UsersApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner repositoryCheck(UserRepository userRepository) {
+		return (args) -> {
+			User user = new User("fName1", "sName1", "mName1", "email1@email.com");
+			userRepository.save(user);
+
+			user = new User("fName2", "sName2", "mName2", "email2@email.com");
+			userRepository.save(user);
+
+			for (User repoUser: userRepository.findAll()) {
+				System.out.println("user # " + repoUser.toString());
+			}
+		};
+	}
 }
