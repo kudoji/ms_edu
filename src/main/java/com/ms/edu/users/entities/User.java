8490a916afe3146ddc5bbad6df28c5cabaee57d9
Your Name <you@example.com>
2023-12-20 17:15:11 +0300

    1.7 getters for user entity

--- src/main/java/com/ms/edu/users/entities/User.java
+++ src/main/java/com/ms/edu/users/entities/User.java
@@ -9,6 +9,27 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     private String secondName;
     private String middleName;
     private String email;
