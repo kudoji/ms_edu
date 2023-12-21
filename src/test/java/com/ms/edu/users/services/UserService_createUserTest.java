6fce55cdb436179e6144ae0180fe7b80de4e758e
ms_edu <ms_edu@example.com>
2023-12-21 09:58:46 +0300

    1.8 UserService_createUserTest optimizations

--- src/test/java/com/ms/edu/users/services/UserService_createUserTest.java
+++ src/test/java/com/ms/edu/users/services/UserService_createUserTest.java
@@ -4,20 +4,28 @@ import com.ms.edu.users.entities.User;
 import com.ms.edu.users.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserService_createUserTest {
+    private static UserRepository userRepository;
+    private static UserService userService;
+    private static User user2save;
+    private static User userSaved;
+
+    @BeforeTestClass
+    public static void beforeClass() {
+        userRepository = Mockito.mock(UserRepository.class);
+        userService = new UserService(userRepository);
+
+        user2save = new User("fn", "sn", "mn", "email");
+        userSaved = new User(1L, user2save);
+    }
 
     @Test
-    void validInput_userCreated() {
-        User user2save = new User("fn", "sn", "mn", "email");
-        User userSaved = new User(1L, user2save);
-
-        UserRepository userRepository = Mockito.mock(UserRepository.class);
+    public void validInput_userCreated() {
         Mockito.when(userRepository.save(user2save)).thenReturn(userSaved);
-
-        UserService userService = new UserService(userRepository);
 
         String result = userService.createUser(user2save);
 
