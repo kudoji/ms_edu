a5ba7a98dc670a9d5d23f92ca519e1581a4a0c21
ms_edu <ms_edu@example.com>
2023-12-22 10:36:14 +0300

    1.8 createUser() for UserControllerTest

--- src/test/java/com/ms/edu/users/controllers/UserControllerTest.java
+++ src/test/java/com/ms/edu/users/controllers/UserControllerTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -23,6 +27,31 @@ class UserControllerTest {
     }
 
     @Test
-    void createUser() {
+    void createUser() throws Exception {
+        String request = "{\n" +
+                "    \"firstName\": \"fn1\",\n" +
+                "    \"secondName\": \"sn1\",\n" +
+                "    \"middleName\": \"mn\",\n" +
+                "    \"email\": \"email\"\n" +
+                "}";
+
+        String response = "{\n" +
+                "    \"id\": 3,\n" +
+                "    \"firstName\": \"fn1\",\n" +
+                "    \"secondName\": \"sn1\",\n" +
+                "    \"middleName\": \"mn\",\n" +
+                "    \"email\": \"email\"\n" +
+                "}";
+
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders
+                        .post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request)
+        );
+
+        resultActions
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().json(response));
     }
 }
