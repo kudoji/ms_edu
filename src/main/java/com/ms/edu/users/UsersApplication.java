ccb13a2304937202954b48586c8c0b87d03852c8
ms_edu <ms_edu@example.com>
2023-12-23 13:57:15 +0300

    1.8 user creating during application start is temporarily commented

--- src/main/java/com/ms/edu/users/UsersApplication.java
+++ src/main/java/com/ms/edu/users/UsersApplication.java
@@ -14,18 +14,18 @@ public class UsersApplication {
 		SpringApplication.run(UsersApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner repositoryCheck(UserRepository userRepository) {
-		return (args) -> {
-			User user = new User("fName1", "sName1", "mName1", "email1@email.com");
-			userRepository.save(user);
-
-			user = new User("fName2", "sName2", "mName2", "email2@email.com");
-			userRepository.save(user);
-
-			for (User repoUser: userRepository.findAll()) {
-				System.out.println("user # " + repoUser.toString());
-			}
-		};
-	}
+//	@Bean
+//	CommandLineRunner repositoryCheck(UserRepository userRepository) {
+//		return (args) -> {
+//			User user = new User("fName1", "sName1", "mName1", "email1@email.com");
+//			userRepository.save(user);
+//
+//			user = new User("fName2", "sName2", "mName2", "email2@email.com");
+//			userRepository.save(user);
+//
+//			for (User repoUser: userRepository.findAll()) {
+//				System.out.println("user # " + repoUser.toString());
+//			}
+//		};
+//	}
 }
